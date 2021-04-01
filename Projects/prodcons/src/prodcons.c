8b8d34693773b9bd7321f35c8eb3a622a2c68c5c
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2021-04-01 01:26:05 -0300

    lab5

--- Projects/prodcons/src/prodcons.c
+++ Projects/prodcons/src/prodcons.c
@@ -1,59 +1,76 @@
 #include "system_tm4c1294.h" // CMSIS-Core
 #include "driverleds.h" // device drivers
 #include "cmsis_os2.h" // CMSIS-RTOS
+#include "driverlib/sysctl.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
 
 #define BUFFER_SIZE 8
 
-osThreadId_t produtor_id, consumidor_id;
+osThreadId_t consumidor_id;
 osSemaphoreId_t vazio_id, cheio_id;
 uint8_t buffer[BUFFER_SIZE];
 
-void produtor(void *arg){
-  uint8_t index_i = 0, count = 0;
+void produtor(){
+  static uint8_t index_i = 0;
+  static uint8_t count = 0;
+  GPIOIntClear(GPIO_PORTJ_BASE, GPIO_PIN_0);
   
-  while(1){
-    osSemaphoreAcquire(vazio_id, osWaitForever); // há espaço disponível?
-    buffer[index_i] = count; // coloca no buffer
-    osSemaphoreRelease(cheio_id); // sinaliza um espaço a menos
-    
-    index_i++; // incrementa índice de colocação no buffer
-    if(index_i >= BUFFER_SIZE)
+  if (osSemaphoreAcquire(vazio_id, 0) != osOK) { // há espaço disponível?
+    for(;;);
+  }
+  buffer[index_i] = count; // coloca no buffer
+  if (osSemaphoreRelease(cheio_id) != osOK) { // sinaliza um espaço a menos
+    for(;;);
+  }
+  
+  index_i++; // incrementa índice de colocação no buffer
+  if(index_i >= BUFFER_SIZE) {
       index_i = 0;
+  }
     
-    count++;
-    count &= 0x0F; // produz nova informação
-    osDelay(500);
-  } // while
+  count++;
 } // produtor
 
 void consumidor(void *arg){
   uint8_t index_o = 0, state;
   
   while(1){
-    osSemaphoreAcquire(cheio_id, osWaitForever); // há dado disponível?
+    if (osSemaphoreAcquire(cheio_id, osWaitForever) != osOK) { // há espaço disponível?
+      for(;;);
+    }
     state = buffer[index_o]; // retira do buffer
-    osSemaphoreRelease(vazio_id); // sinaliza um espaço a mais
-    
+    if (osSemaphoreRelease(vazio_id) != osOK) { // sinaliza um espaço a menos
+      for(;;);
+    }    
     index_o++;
     if(index_o >= BUFFER_SIZE) // incrementa índice de retirada do buffer
       index_o = 0;
     
-    LEDWrite(LED4 | LED3 | LED2 | LED1, state); // apresenta informação consumida
-    osDelay(500);
+    LEDWrite(LED4 | LED3 | LED2 | LED1, state & 0x0F); // apresenta informação consumida
   } // while
 } // consumidor
 
 void main(void){
   SystemInit();
-  LEDInit(LED4 | LED3 | LED2 | LED1);
-
+  
   osKernelInitialize();
-
-  produtor_id = osThreadNew(produtor, NULL, NULL);
-  consumidor_id = osThreadNew(consumidor, NULL, NULL);
 
   vazio_id = osSemaphoreNew(BUFFER_SIZE, BUFFER_SIZE, NULL); // espaços disponíveis = BUFFER_SIZE
   cheio_id = osSemaphoreNew(BUFFER_SIZE, 0, NULL); // espaços ocupados = 0
+
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); // Habilita GPIO J (push-button SW1 = PJ0, push-button SW2 = PJ1)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)); // Aguarda final da habilitação
+  
+  GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0); // push-buttons SW1 e SW2 como entrada
+  GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  
+  GPIOIntRegister(GPIO_PORTJ_BASE, produtor);
+  GPIOIntTypeSet(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_FALLING_EDGE);
+  GPIOIntEnable(GPIO_PORTJ_BASE, GPIO_PIN_0);
+
+  LEDInit(LED4 | LED3 | LED2 | LED1);
+
+  consumidor_id = osThreadNew(consumidor, NULL, NULL);
   
   if(osKernelGetState() == osKernelReady)
     osKernelStart();
